75ccd2a5ac4eac1954af9b286583a1abccb20866
webrotate360 <webrotate360@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-11 04:20:43 +0000

    Initial plugin commit upon approval by WP. Version 2.0.
    git-svn-id: https://plugins.svn.wordpress.org/webrotate-360-product-viewer/trunk@695664 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== WebRotate 360 Product Viewer for WordPress ===
+Contributors: sergey z., dimitri bir
+Donate link: http://www.webrotate360.com/360-product-viewer.html
+Tags: 360 viewer, 360 product viewer, 360 views, 360 product views, image zoom, 3D viewer, pano viewer, product zoom, 360 degree panorama, 360 product spin, html5, QTVR, 360 product photography, virtual tour, 360 degree photo
+Requires at least: 3.0.0
+Tested up to: 3.5.1
+Stable tag: 2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Embed 360 product views and 360-degree panoramic views created with WebRotate 360 Product Viewer in your WordPress posts and pages.
+
+== Description ==
+
+WebRotate 360 Product Viewer for WordPress is a free plugin that can be used to integrate advanced 360 product views and 360-degree panoramic views created with <a href="http://www.webrotate360.com/360-product-viewer.html">WebRotate 360 Product Viewer</a>
+To see a demonstration of this plugin, please visit our <a href="http://www.360-product-views.com/wordpress/">WordPress demo page</a>.
+
+<h4>Main plugin features include:</h4>
+
+* Ability to add multiple 360 product views on the same page or post.
+* Gallery mode to showcase your 360 views inside an expandable popup gallery.
+* Dynamic 360 viewer resizing in responsive WordPress themes.
+* Works on iPad, iPhone, Android smartphones and browsers with or without HTML5.
+* Variety of settings to control playback, speed, rotation, inertia, presentation, and more.
+* Extensive hot-spot and image annotation support with granular control.
+* Deep image zoom using an extra set of high resolution images.
+* All configuration is done via a separate XML file.
+* Free desktop software for quick configuration and publishing of your 360 views is available.
+* No server side scripts required - all client / browser based.
+
+
+== Installation ==
+
+1. Install WebRotate 360 Product Viewer for WordPress by either installing directly from your WordPress Admin area or download the module manually and copy unzipped webrotate360 folder into your wp-content/plugins.
+2. Activate the module via the Plugins page in your WordPress Admin.
+3. Upload your 360 views via ftp to your web server and note the location of xml configuration file(s) in the uploaded folder(s) as you will need it for the shortcodes as shown below.
+4. Simply add the following line inside your post or page where you would like to see the 360 view embedded, substituting name, width, height and config with your own values.
+
+[wr360embed name="yourviewname" width="100%" height="450px" config="/wordpress/360-assets/sampleshoe/config.xml"]
+
+5. To integrate via popup, use the following shortcode (replace name, width, height and config with your own values):
+
+[wr360popup name="yourviewname" width="620" height="450" config="/wordpress/360-assets/sampleshoe2/config.xml"]your thumbnail image or text[/wr360popup]
+
+If you need to show a gallery of your 360 views via a popup, add multiple wr360popup shortcodes for each of your 360 views with an extra parameter called gallery:
+
+[wr360popup name="yourviewname1" gallery="true" width="620" height="450" config="/wordpress/360-assets/sampleshoe2/config.xml"]your thumbnail image or text[/wr360popup]
+[wr360popup name="yourviewname2" gallery="true" width="400" height="400" config="/wordpress/360-assets/sampleshoe3/config.xml"]your thumbnail image or text[/wr360popup]
+
+== Uninstall ==
+
+Deactivate WebRotate 360 Product Viewer for WordPress via the plugins page in your WordPress Admin.
---
+++ webrotate360.php
@@ -0,0 +1,116 @@
+<?php
+/*
+Plugin Name: WebRotate 360 Product Viewer for WordPress
+Plugin URI: http://www.360-product-views.com/wordpress/
+Description: WebRotate 360 Product Viewer WordPress Integration
+Version: 2.0
+Author: WebRotate 360 LLC
+Author URI: http://www.webrotate360.com
+License: GPLv2
+*/
+?>
+<?php
+
+    add_shortcode("wr360embed", "wr360embed_shortcode_hanlder");
+    add_shortcode("wr360popup", "wr360popup_shortcode_hanlder");
+
+    add_action("wp_enqueue_scripts", "add_action_webrotate_styles");
+    add_filter("the_content", "add_filter_webrotate_embed_viewer");
+
+    function wr360popup_shortcode_hanlder($atts, $content)
+    {
+        $extract = shortcode_atts(
+            array(
+                'name' => '',
+                'width' => '',
+                'height' => '',
+                'rootpath' => '',
+                'config' => '',
+                'gallery' => 'false'
+            ), $atts);
+
+        $replace  = "";
+        if ( $extract["gallery"] == "true")
+        {
+            $replace .= "<a id='%s' href='%s?config=%s&root=%s&iframe=true&width=%s&height=%s' rel=prettyPhoto[iframe]>";
+        }
+        else
+        {
+            $replace .= "<a id='%s' href='%s?config=%s&root=%s&iframe=true&width=%s&height=%s' rel=prettyPhoto>";
+        }
+
+        $replace .= $content;
+        $replace .= "</a>";
+
+        return (sprintf(
+            $replace,
+            'wr360' . $extract["name"],
+            plugins_url("webrotate360/viewloader.html"),
+            urlencode($extract["config"]),
+            urlencode($extract["rootpath"]),
+            $extract["width"],
+            $extract["height"],
+            $extract["width"],
+            $extract["height"]
+        ));
+    }
+
+    function wr360embed_shortcode_hanlder($atts, $content)
+    {
+        $extract = shortcode_atts(
+                array(
+                    'name' => '',
+                    'width' => '',
+                    'height' => '',
+                    'rootpath' => '',
+                    'config' => ''
+                    ), $atts);
+
+        $replace  = "";
+        $replace .= "<div id='%s' class='webrotate360' style='width:%s; height:%s;'>";
+        $replace .= "<div id='%s' class='wr360_player'></div></div>";
+        $replace .= "<script language='javascript' type='text/javascript'>";
+        $replace .= "jQuery(document).ready(function(){jQuery('#' + '%s').rotator({graphicsPath: '%s', licenseFileURL: '%s', rootPath:'%s', configFileURL:'%s'});});";
+        $replace .= "</script>";
+
+        return (sprintf(
+            $replace,
+            'wr360' . $extract["name"],
+            $extract["width"],
+            $extract["height"],
+            'wr360' . $extract["name"].'_playerid',
+            'wr360' . $extract["name"].'_playerid',
+            plugins_url("webrotate360/imagerotator/html/img/basic"),
+            plugins_url("webrotate360/license.lic"),
+            $extract["rootpath"],
+            $extract["config"]));
+    }
+
+
+    function add_action_webrotate_styles()
+    {
+        wp_enqueue_script   ("jquery");
+
+        wp_register_script  ("prettyphotojs", plugins_url("prettyphoto/js/jquery.prettyPhoto.js", __FILE__));
+        wp_register_style   ("prettyphotocss", plugins_url("prettyphoto/css/prettyphoto.css", __FILE__));
+        wp_enqueue_script   ("prettyphotojs");
+        wp_enqueue_style    ("prettyphotocss");
+
+        wp_register_script  ("wr360wpscript", plugins_url("webrotate360.js", __FILE__));
+        wp_enqueue_script   ("wr360wpscript");
+
+        wp_register_script  ("wr360script", plugins_url("imagerotator/html/js/imagerotator.js", __FILE__));
+		wp_register_style   ("wr360style", plugins_url("imagerotator/html/css/basic.css", __FILE__));
+        wp_enqueue_style    ("wr360style");
+        wp_enqueue_script   ("wr360script");
+    }
+
+    function add_filter_webrotate_embed_viewer($content)
+    {
+        $content .= "<script language='javascript' type='text/javascript'>";
+        $content .= "function getWR360License(){return '" . plugins_url("webrotate360/license.lic") . "';}";
+        $content .= "function getWR360GraphicsPath(){return '" . plugins_url("webrotate360/imagerotator/html/img/basic") . "';}";
+        $content .= "</script>";
+		return ($content);
+    }
+?>
